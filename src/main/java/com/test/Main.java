533be6ff1b8334594e02028844841c7dfd3248d2
Lee So Ra <lalila77@naver.com>
2024-07-29 16:31:50 +0900

    ok

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,8 +1,22 @@
 package com.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Have a good day22");
+        // Student student = new Student();/
+        // System.out.println(student.getStudentId());
+        // System.out.println(student.getStudentName());
+        // student.setStudentId("1");
+        // student.setStudentName("이순신");
+
+        ApplicationContext context =
+            new ClassPathXmlApplicationContext("Beans.xml");
+        Student student =
+            context.getBean("student", Student.class);
+            student.setStudentId("2");
+            student.setStudentName("강감찬");
+            System.out.println(student.getStudentName());
     }
 }
